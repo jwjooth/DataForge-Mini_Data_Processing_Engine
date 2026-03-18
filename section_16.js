2722a212fbb0dd0a1f9b63af5fbebb90199a747a
Jordan Ferdinand <253198146+jordan-ferdinand_IMSI@users.noreply.github.com>
2026-03-19 00:22:34 +0700

    adding something

---
+++ section_16.js
@@ -0,0 +1,21 @@
+export function dynamicOperations(){
+  const getProperty = (obj, key) => {
+
+  }
+
+  const setProperty = (obj, key, value) => {
+
+  }
+
+  const hasProperty = (obj, key) => {
+
+  }
+
+  const deleteProperty = (obj, key) => {
+
+  }
+
+  const listKeys = (obj) => {
+      
+  }
+}
